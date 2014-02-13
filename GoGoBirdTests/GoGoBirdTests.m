385df7a9c95fb16dd514317040dd5f9152912fdc
CODERLAB <CODERLAB@coderlab.local>
2014-02-13 10:16:50 +0400

    Initial Commit

---
+++ GoGoBirdTests/GoGoBirdTests.m
@@ -0,0 +1,34 @@
+//
+//  GoGoBirdTests.m
+//  GoGoBirdTests
+//
+//  Created by CODERLAB on 13.02.14.
+//  Copyright (c) 2014 studio76. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoGoBirdTests : XCTestCase
+
+@end
+
+@implementation GoGoBirdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
